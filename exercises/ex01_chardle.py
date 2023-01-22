476dd2716b3cddce38c336c6434a4776dc3bbc45
Isabel Marshall <ibmarsh@unc.edu>
2023-01-22 12:25:42 -0500

    ex01 - final submission

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,46 +1,47 @@
 """EX 01 - Chardle - A cute step toward Wordle."""
 __author__ = "730610651"
 
-five_letter_word : str = input("Enter a 5-character word: ")
+five_letter_word: str = input("Enter a 5-character word: ")
 
 if len(five_letter_word) != 5:
     print("Error: Word must contain 5 characters")
     exit()
 
-single_character : str = input("Enter a single character: ")
-print("Searching for " + single_character + " in " + five_letter_word)
+single_character: str = input("Enter a single character: ")
 
 if len(single_character) != 1:
     print("Error: Character must be a single character.")
     exit()
 
+print("Searching for " + single_character + " in " + five_letter_word)
+
 match_count = 0
 
-if(single_character == five_letter_word[0]):
+if (single_character == five_letter_word[0]):
     print(single_character + " found at index 0")
     match_count = 1
 
-if(single_character == five_letter_word[1]):
+if (single_character == five_letter_word[1]):
     print(single_character + " found at index 1")
     match_count = match_count + 1
 
-if(single_character == five_letter_word[2]):
+if (single_character == five_letter_word[2]):
     print(single_character + " found at index 2")
     match_count = match_count + 1
 
-if(single_character == five_letter_word[3]):
+if (single_character == five_letter_word[3]):
     print(single_character + " found at index 3")
     match_count = match_count + 1
 
-if(single_character == five_letter_word[4]):
+if (single_character == five_letter_word[4]):
     print(single_character + " found at index 4")
     match_count = match_count + 1
 
-if(match_count == 0):
+if (match_count == 0):
     print("No instances of " + single_character + " found in " + five_letter_word)
 
-if(match_count == 1):
+if (match_count == 1):
     print("1 instance of " + single_character + " found in " + five_letter_word)
 
-if(match_count >= 2):
+if (match_count >= 2):
     print(str(match_count) + " instances of " + single_character + " found in " + five_letter_word)
