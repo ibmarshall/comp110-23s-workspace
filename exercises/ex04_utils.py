c756227a03b7faf8d672f098ffd8949700bf0f5a
Isabel Marshall <ibmarsh@unc.edu>
2023-03-01 16:42:53 -0500

    final submission

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,10 +1,13 @@
-"""Testing Utility Functions"""
+"""Testing Utility Functions."""
 __author__ = "730610651"
 
+
 def all(list1: list[int], a: int) -> bool:
-    """Given a list and an integer, do all of the integers in the list match the given integer"""
+    """Check to see if a is equal to each index of list1."""
     list1_idx: int = 0
-    list1true: bool = False
+
+    if len(list1) == 0:
+        return False
 
     while list1_idx < len(list1) - 1:
         if list1[list1_idx] != a:
@@ -12,21 +15,30 @@ def all(list1: list[int], a: int) -> bool:
         list1_idx += 1
     return True
 
-def max(list1: list[int]) -> int:
+
+def max(input: list[int]) -> int:
+    """If no length is input, output ValueError."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     
+    """Check if value at certain idx is larger than original max."""
     list1_idx: int = 0
-    
-    while list1_idx <= len(list1) - 1:
-        if list1[list1_idx] < list1[list1_idx + 1]:
-            list1_idx += 1
-    return list1[list1_idx]
+    max = input[0]
+    while list1_idx <= len(input) - 1:
+        if max < input[list1_idx]:
+            max = input[list1_idx]
+        list1_idx += 1
+    return max
+
 
 def is_equal(list1: list[int], list2: list[int]) -> bool:
+    """Check if length of lists are the same."""
+    if len(list1) != len(list2):
+        return False
+    
+    """check if the values match at each index"""
     list_idx: int = 0
-
-    while list_idx < len(list1) and len(list2):
+    while list_idx < len(list1) - 1:
         if list1[list_idx] == list2[list_idx]:
             list_idx += 1
         else:
